db5508e5e5cb9def1bffd5a29e53d802df3221b2
Adam <golota.adam@gmail.com>
2026-05-18 09:47:17 +0300

    feat: add basic helper functions

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 console.log("Hello, World!");
+
+function sum(a, b) {
+  return a + b;
+}
+
+function celsiusToFahrenheit(c) {
+  return (c * 9) / 5 + 32;
+}
+
+function fahrenheitToCelsius(f) {
+  return ((f - 32) * 5) / 9;
+}
+
+console.log("2 + 3 =", sum(2, 3));
+console.log("100°C in Fahrenheit:", celsiusToFahrenheit(100));
+console.log("32°F in Celsius:", fahrenheitToCelsius(32));
